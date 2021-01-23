30eab73609456e984dc026c82103a5b9cbbab5e2
SashaCN <sashacn228@gmail.com>
2021-01-24 00:47:53 +0200

    Code online

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let htmlBox = document.querySelector(".html"),
+    cssBox = document.querySelector(".css"),
+    jsBox = document.querySelector(".js"),
+    viewBox = document.querySelector(".view"),
+    cssDocument
+
+htmlBox.onchange = change
+cssBox.onchange = change
+jsBox.onchange = change
+
+function change (){
+  console.log("+")
+  viewBox.innerHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <style>
+        ${cssBox.value}
+      </style>
+      ${htmlBox.value}
+    </body>
+    </html>`
+  cssDocument = document.querySelector("style")
+  eval(jsBox.value)
+}
+
+
